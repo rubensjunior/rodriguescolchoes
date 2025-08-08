2311ddd412f9d73969f14527c6963c3cbec5c459
rbentes <rbntes@gmail.com>
2025-08-08 12:53:22 -0400

    Adicionado pagina adicionar veiculo, adicionado formulario, corrigido navegacao no layout mobile. inicio da api cadastrar veiculo.

---
+++ public/dmxAppConnect/config.js
@@ -0,0 +1,41 @@
+dmx.config({
+  "inicialcolaborador": {
+    "data_detail_veiculo": {
+      "meta": [
+        {
+          "type": "number",
+          "name": "id_motorista"
+        },
+        {
+          "type": "number",
+          "name": "id_veiculo"
+        },
+        {
+          "type": "text",
+          "name": "base64_placa"
+        },
+        {
+          "type": "text",
+          "name": "nome_colaborador"
+        },
+        {
+          "type": "text",
+          "name": "cpf_colaborador"
+        },
+        {
+          "type": "text",
+          "name": "placa_veiculo"
+        },
+        {
+          "type": "text",
+          "name": "modelo_veiculo"
+        },
+        {
+          "type": "text",
+          "name": "cor_veiculo"
+        }
+      ],
+      "outputType": "array"
+    }
+  }
+});
