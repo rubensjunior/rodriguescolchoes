f6a9f97286c4425323f877acb76352a1a15028f0
rbentes <rbntes@gmail.com>
2025-08-21 11:57:27 -0400

    Criaçao de fila com log completa. update incompleto de fila com log. pagina de produtos iniciada.

--- public/dmxAppConnect/config.js
+++ public/dmxAppConnect/config.js
@@ -37,5 +37,110 @@ dmx.config({
       ],
       "outputType": "array"
     }
+  },
+  "filainauguracoes": {
+    "data_fila": {
+      "meta": [
+        {
+          "type": "text",
+          "name": "id_inauguracao"
+        },
+        {
+          "type": "datetime",
+          "name": "created_at_inauguracao"
+        },
+        {
+          "type": "text",
+          "name": "nome_loja"
+        },
+        {
+          "type": "text",
+          "name": "cidade"
+        },
+        {
+          "type": "text",
+          "name": "uf"
+        },
+        {
+          "type": "text",
+          "name": "endereco_completo"
+        },
+        {
+          "type": "text",
+          "name": "complemento"
+        },
+        {
+          "type": "text",
+          "name": "cep"
+        },
+        {
+          "type": "text",
+          "name": "cod_filial"
+        },
+        {
+          "type": "datetime",
+          "name": "data_inicio"
+        },
+        {
+          "type": "text",
+          "name": "status"
+        },
+        {
+          "type": "datetime",
+          "name": "data_fim"
+        },
+        {
+          "type": "number",
+          "name": "total_senhas"
+        },
+        {
+          "type": "number",
+          "name": "regional"
+        },
+        {
+          "type": "text",
+          "name": "bairro"
+        },
+        {
+          "name": "produto",
+          "type": "array",
+          "sub": [
+            {
+              "type": "text",
+              "name": "id_inaug_produto"
+            },
+            {
+              "type": "datetime",
+              "name": "created_at_inaug_produto"
+            },
+            {
+              "type": "number",
+              "name": "produto_inaug_id"
+            },
+            {
+              "type": "number",
+              "name": "inauguracao_id"
+            },
+            {
+              "type": "number",
+              "name": "unidades_disponiveis"
+            },
+            {
+              "type": "number",
+              "name": "limite_por_pessoa"
+            },
+            {
+              "type": "number",
+              "name": "quantidade_consumida"
+            },
+            {
+              "type": "text",
+              "name": "status"
+            }
+          ]
+        }
+      ],
+      "outputType": "array"
+    }
   }
 });
